bd7dfca4c437eaf3a4bdc2fb64db6f5aa1ef830f
Abraham Bueno <abueno@localhost.localdomain>
2019-02-14 15:57:09 -0600

    Initial setup of GDPController

--- src/main/java/com/abraham/gdp/GDPController.java
+++ src/main/java/com/abraham/gdp/GDPController.java
@@ -1,4 +1,27 @@
 package com.abraham.gdp;
 
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
 public class GDPController {
+    private final GDPRepository gdprepos;
+    private final RabbitTemplate rt;
+
+    public GDPController(GDPRepository gdprepos, RabbitTemplate rt) {
+        this.gdprepos = gdprepos;
+        this.rt = rt;
+    }
+
+    @GetMapping("/names")
+
+    @GetMapping("/economy")
+
+    @GetMapping("/total")
+
+    @GetMapping("/total/{name}")
 }
