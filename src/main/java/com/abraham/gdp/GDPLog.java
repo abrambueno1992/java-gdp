eb50bc278d7e0954e3f944289655535a30ddb0e3
Abraham Bueno <abueno@localhost.localdomain>
2019-02-14 15:28:24 -0600

    GDPLog class and GdpApplication setup has been complete.

--- src/main/java/com/abraham/gdp/GDPLog.java
+++ src/main/java/com/abraham/gdp/GDPLog.java
@@ -1,4 +1,18 @@
 package com.abraham.gdp;
 
-public class GDPLog {
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class GDPLog implements Serializable {
+    private final String text;
+    private final String formattedDate;
+    public GDPLog(String text) {
+        this.text = text;
+        Date date = new Date();
+        String strDateFormat = "yyyy-MM-dd hh:mm:ss a";
+        DateFormat dateFormat = new SimpleDateFormat(strDateFormat);
+        formattedDate = dateFormat.format(date);
+    }
 }
