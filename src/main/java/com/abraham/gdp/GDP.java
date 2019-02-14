94a9f566535d5406e7f057fb5a6016972265400f
Abraham Bueno <abueno@localhost.localdomain>
2019-02-14 15:05:36 -0600

    GDP class initialized

--- src/main/java/com/abraham/gdp/GDP.java
+++ src/main/java/com/abraham/gdp/GDP.java
@@ -1,4 +1,25 @@
 package com.abraham.gdp;
 
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
 public class GDP {
+    private @Id @GeneratedValue Long id;
+    private String country;
+    private Long gdp;
+
+    public GDP() {
+        // default constructor
+    }
+
+    public GDP(String country, Long gdp) {
+        this.country = country;
+        this.gdp = gdp;
+    }
 }
